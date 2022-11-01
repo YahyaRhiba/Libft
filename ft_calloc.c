a05fa8d6d6f43a280d967d584370ce045d4e5839
yrhiba <yahyarhiba@gmail.com>
2022-11-01 01:40:24 +0100

    modified:   ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,30 +6,23 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 13:00:03 by yrhiba            #+#    #+#             */
-/*   Updated: 2022/10/08 13:24:37 by yrhiba           ###   ########.fr       */
+/*   Updated: 2022/11/01 01:39:22 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_b_zero(void *s, size_t n)
-{
-	unsigned char	*tmp;
-	size_t			i;
-
-	tmp = (unsigned char *) s;
-	i = 0;
-	while (i < n)
-		tmp[i++] = '\0';
-}
-
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*rtn;
+	size_t	total_size;
 
-	rtn = malloc(size * count);
+	total_size = count * size;
+	if ((total_size / count) != size)
+		return (NULL);
+	rtn = malloc(total_size);
 	if (!rtn)
 		return (NULL);
-	ft_b_zero(rtn, size * count);
+	ft_bzero(rtn, total_size);
 	return (rtn);
 }
